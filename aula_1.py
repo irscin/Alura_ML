069585807e61e43829b91c1004e0b5c36b908454
Italo Soares <irs@cin.ufpe.br>
2018-11-20 18:28:37 -0300

    Fim da aula 1

--- aula_1.py
+++ aula_1.py
@@ -1,4 +1,5 @@
 from sklearn.naive_bayes import MultinomialNB
+#Definindo instâncias para treino
 porco1=[1,1,0]
 porco2=[1,1,0]
 porco3=[1,1,0]
@@ -7,7 +8,11 @@ cachorro2=[0,1,1]
 cachorro3=[0,1,1]
 dados=[porco1,porco2,porco3,cachorro1,cachorro2,cachorro3]
 marcacoes=[1,1,1,-1,-1,-1]
-test=[[1,1,1],[1,0,0]]
+#Dataset não classificado
+test=[[1,1,1],[1,0,0],[1,0,1]]
+#Resultado esperado das classificações
+marcacoes_teste=[-1,1,-1]
 modelo = MultinomialNB()
 modelo.fit(dados, marcacoes)
-print(modelo.predict(test))
+result=modelo.predict(test)
+print(result-marcacoes_teste)
